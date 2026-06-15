c791e356a99df01f4e7438d4dd52d61b39be4e82
c-doubley <861639584@qq.com>
2026-06-15 09:25:13 +0800

    Optimize ModuleMVOLE WHT benchmark

--- ModuleMVOLE.h
+++ ModuleMVOLE.h
@@ -45,6 +45,7 @@ namespace osuCrypto
     struct ModuleMVOLEBaseState
     {
         typename Ctx::template Vec<F> rho;
+        typename Ctx::template Vec<F> rhoWht;
         typename Ctx::template Vec<F> s;
         typename Ctx::template Vec<F> e;
         typename Ctx::template Vec<F> b;
@@ -81,6 +82,34 @@ namespace osuCrypto
         for (u64 i = 0; i < N; ++i)
         {
             for (u64 j = 0; j < N; ++j)
+            {
+                F prod;
+                F sum;
+                ctx.mul(prod, lhs[i], rhs[j]);
+                ctx.plus(sum, out[i ^ j], prod);
+                out[i ^ j] = sum;
+            }
+        }
+    }
+
+    template<typename F, typename Ctx>
+    void moduleMvoleRingMulSparseRhs(
+        const typename Ctx::template Vec<F>& lhs,
+        const typename Ctx::template Vec<F>& rhs,
+        typename Ctx::template Vec<F>& out,
+        Ctx& ctx)
+    {
+        auto N = static_cast<u64>(lhs.size());
+        F zero{};
+        ctx.resize(out, N);
+        ctx.zero(out.begin(), out.end());
+
+        for (u64 j = 0; j < N; ++j)
+        {
+            if (ctx.eq(rhs[j], zero))
+                continue;
+
+            for (u64 i = 0; i < N; ++i)
             {
                 F prod;
                 F sum;
@@ -132,11 +161,14 @@ namespace osuCrypto
         ctx.resize(state.rho, params.N);
         for (u64 j = 0; j < params.N; ++j)
             ctx.fromBlock(state.rho[j], prng.get());
+        ctx.resize(state.rhoWht, params.N);
+        ctx.copy(state.rho.begin(), state.rho.end(), state.rhoWht.begin());
+        wht<F, Ctx>(state.rhoWht, static_cast<int>(params.N));
 
         moduleMvoleSampleSparse<F, Ctx>(state.s, params.N, params.t, prng, ctx);
         moduleMvoleSampleSparse<F, Ctx>(state.e, params.N, params.t, prng, ctx);
 
-        moduleMvoleRingMul<F, Ctx>(state.rho, state.s, rhoS, ctx);
+        moduleMvoleRingMulSparseRhs<F, Ctx>(state.rho, state.s, rhoS, ctx);
         ctx.resize(state.b, params.N);
         for (u64 j = 0; j < params.N; ++j)
             ctx.plus(state.b[j], rhoS[j], state.e[j]);
@@ -213,23 +245,28 @@ namespace osuCrypto
         Ctx ctx = {})
     {
         using VecF = typename Ctx::template Vec<F>;
-        VecF rhoV, row0;
+        VecF row0, rowMask;
 
         ctx.resize(p0.x, params.N);
         ctx.copy(gen.base.b.begin(), gen.base.b.end(), p0.x.begin());
         wht<F, Ctx>(p0.x, static_cast<int>(params.N));
 
         p0.Z0.resize(params.m, params.N);
+        ctx.resize(row0, params.N);
+        ctx.resize(rowMask, params.N);
 
         for (u64 h = 0; h < params.m; ++h)
         {
-            ctx.resize(row0, params.N);
-            moduleMvoleRingMul<F, Ctx>(gen.base.rho, gen.rowMasks[h].V, rhoV, ctx);
-
-            for (u64 j = 0; j < params.N; ++j)
-                ctx.plus(row0[j], rhoV[j], gen.rowMasks[h].U[j]);
-
+            ctx.copy(gen.rowMasks[h].V.begin(), gen.rowMasks[h].V.end(), row0.begin());
+            ctx.copy(gen.rowMasks[h].U.begin(), gen.rowMasks[h].U.end(), rowMask.begin());
             wht<F, Ctx>(row0, static_cast<int>(params.N));
+            wht<F, Ctx>(rowMask, static_cast<int>(params.N));
+            for (u64 j = 0; j < params.N; ++j)
+            {
+                F prod;
+                ctx.mul(prod, gen.base.rhoWht[j], row0[j]);
+                ctx.plus(row0[j], prod, rowMask[j]);
+            }
 
             for (u64 j = 0; j < params.N; ++j)
                 p0.Z0(h, j) = row0[j];
@@ -244,21 +281,26 @@ namespace osuCrypto
         Ctx ctx = {})
     {
         using VecF = typename Ctx::template Vec<F>;
-        VecF rhoW0, row1;
+        VecF row1, rowMask;
 
         ctx.resize(p1.Delta, params.m);
         ctx.copy(gen.Delta.begin(), gen.Delta.end(), p1.Delta.begin());
         p1.Z1.resize(params.m, params.N);
+        ctx.resize(row1, params.N);
+        ctx.resize(rowMask, params.N);
 
         for (u64 h = 0; h < params.m; ++h)
         {
-            ctx.resize(row1, params.N);
-            moduleMvoleRingMul<F, Ctx>(gen.base.rho, gen.rowMasks[h].W0, rhoW0, ctx);
-
-            for (u64 j = 0; j < params.N; ++j)
-                ctx.plus(row1[j], rhoW0[j], gen.rowMasks[h].W1[j]);
-
+            ctx.copy(gen.rowMasks[h].W0.begin(), gen.rowMasks[h].W0.end(), row1.begin());
+            ctx.copy(gen.rowMasks[h].W1.begin(), gen.rowMasks[h].W1.end(), rowMask.begin());
             wht<F, Ctx>(row1, static_cast<int>(params.N));
+            wht<F, Ctx>(rowMask, static_cast<int>(params.N));
+            for (u64 j = 0; j < params.N; ++j)
+            {
+                F prod;
+                ctx.mul(prod, gen.base.rhoWht[j], row1[j]);
+                ctx.plus(row1[j], prod, rowMask[j]);
+            }
 
             for (u64 j = 0; j < params.N; ++j)
                 p1.Z1(h, j) = row1[j];
@@ -332,19 +374,154 @@ namespace osuCrypto
 
     struct ModuleMVOLEBenchResult
     {
+        double genBaseSeconds = 0;
+        double directMaskSeconds = 0;
         double genSeconds = 0;
         double expandP0Seconds = 0;
         double expandP1Seconds = 0;
+        double xWhtSeconds = 0;
+        double z0RowWhtSeconds = 0;
+        double z1RowWhtSeconds = 0;
         double verifySeconds = 0;
+        double microWhtOneSeconds = 0;
+        double microWhtMSeconds = 0;
         bool ok = false;
     };
 
-    inline double moduleMvoleTotalSeconds(const ModuleMVOLEBenchResult& result)
+    inline double moduleMvoleTotalNoVerifySeconds(const ModuleMVOLEBenchResult& result)
     {
         return result.genSeconds
             + result.expandP0Seconds
-            + result.expandP1Seconds
+            + result.expandP1Seconds;
+    }
+
+    inline double moduleMvoleTotalSeconds(const ModuleMVOLEBenchResult& result)
+    {
+        return moduleMvoleTotalNoVerifySeconds(result)
             + result.verifySeconds;
+    }
+
+    template<typename F, typename Ctx>
+    void moduleMvoleExpandP0Timed(
+        ModuleMVOLEP0Output<F, Ctx>& p0,
+        const ModuleMVOLEParams& params,
+        const ModuleMVOLEGenState<F, Ctx>& gen,
+        ModuleMVOLEBenchResult& timings,
+        Ctx ctx = {})
+    {
+        using VecF = typename Ctx::template Vec<F>;
+        VecF row0, rowMask;
+
+        ctx.resize(p0.x, params.N);
+        ctx.copy(gen.base.b.begin(), gen.base.b.end(), p0.x.begin());
+
+        auto start = omp_get_wtime();
+        wht<F, Ctx>(p0.x, static_cast<int>(params.N));
+        auto end = omp_get_wtime();
+        timings.xWhtSeconds += end - start;
+
+        p0.Z0.resize(params.m, params.N);
+        ctx.resize(row0, params.N);
+        ctx.resize(rowMask, params.N);
+
+        for (u64 h = 0; h < params.m; ++h)
+        {
+            ctx.copy(gen.rowMasks[h].V.begin(), gen.rowMasks[h].V.end(), row0.begin());
+            ctx.copy(gen.rowMasks[h].U.begin(), gen.rowMasks[h].U.end(), rowMask.begin());
+
+            start = omp_get_wtime();
+            wht<F, Ctx>(row0, static_cast<int>(params.N));
+            wht<F, Ctx>(rowMask, static_cast<int>(params.N));
+            end = omp_get_wtime();
+            timings.z0RowWhtSeconds += end - start;
+
+            for (u64 j = 0; j < params.N; ++j)
+            {
+                F prod;
+                ctx.mul(prod, gen.base.rhoWht[j], row0[j]);
+                ctx.plus(row0[j], prod, rowMask[j]);
+            }
+
+            for (u64 j = 0; j < params.N; ++j)
+                p0.Z0(h, j) = row0[j];
+        }
+    }
+
+    template<typename F, typename Ctx>
+    void moduleMvoleExpandP1Timed(
+        ModuleMVOLEP1Output<F, Ctx>& p1,
+        const ModuleMVOLEParams& params,
+        const ModuleMVOLEGenState<F, Ctx>& gen,
+        ModuleMVOLEBenchResult& timings,
+        Ctx ctx = {})
+    {
+        using VecF = typename Ctx::template Vec<F>;
+        VecF row1, rowMask;
+
+        ctx.resize(p1.Delta, params.m);
+        ctx.copy(gen.Delta.begin(), gen.Delta.end(), p1.Delta.begin());
+        p1.Z1.resize(params.m, params.N);
+        ctx.resize(row1, params.N);
+        ctx.resize(rowMask, params.N);
+
+        for (u64 h = 0; h < params.m; ++h)
+        {
+            ctx.copy(gen.rowMasks[h].W0.begin(), gen.rowMasks[h].W0.end(), row1.begin());
+            ctx.copy(gen.rowMasks[h].W1.begin(), gen.rowMasks[h].W1.end(), rowMask.begin());
+
+            auto start = omp_get_wtime();
+            wht<F, Ctx>(row1, static_cast<int>(params.N));
+            wht<F, Ctx>(rowMask, static_cast<int>(params.N));
+            auto end = omp_get_wtime();
+            timings.z1RowWhtSeconds += end - start;
+
+            for (u64 j = 0; j < params.N; ++j)
+            {
+                F prod;
+                ctx.mul(prod, gen.base.rhoWht[j], row1[j]);
+                ctx.plus(row1[j], prod, rowMask[j]);
+            }
+
+            for (u64 j = 0; j < params.N; ++j)
+                p1.Z1(h, j) = row1[j];
+        }
+    }
+
+    template<typename F, typename Ctx>
+    double moduleMvoleMicrobenchWhtOne(
+        const ModuleMVOLEParams& params,
+        PRNG& prng,
+        Ctx ctx = {})
+    {
+        typename Ctx::template Vec<F> vec;
+        ctx.resize(vec, params.N);
+        for (u64 j = 0; j < params.N; ++j)
+            ctx.fromBlock(vec[j], prng.get());
+
+        auto start = omp_get_wtime();
+        wht<F, Ctx>(vec, static_cast<int>(params.N));
+        auto end = omp_get_wtime();
+        return end - start;
+    }
+
+    template<typename F, typename Ctx>
+    double moduleMvoleMicrobenchWhtM(
+        const ModuleMVOLEParams& params,
+        PRNG& prng,
+        Ctx ctx = {})
+    {
+        typename Ctx::template Vec<F> vec;
+        ctx.resize(vec, params.N);
+
+        auto start = omp_get_wtime();
+        for (u64 h = 0; h < params.m; ++h)
+        {
+            for (u64 j = 0; j < params.N; ++j)
+                ctx.fromBlock(vec[j], prng.get());
+            wht<F, Ctx>(vec, static_cast<int>(params.N));
+        }
+        auto end = omp_get_wtime();
+        return end - start;
     }
 
     template<typename F, typename Ctx>
@@ -358,17 +535,27 @@ namespace osuCrypto
         ModuleMVOLEBenchResult timings;
 
         auto start = omp_get_wtime();
-        moduleMvoleGenDirect<F, Ctx>(gen, params, prng, ctx);
+        gen.base = moduleMvoleGenBase<F, Ctx>(params, prng, ctx);
         auto end = omp_get_wtime();
-        timings.genSeconds = end - start;
+        timings.genBaseSeconds = end - start;
 
         start = omp_get_wtime();
-        moduleMvoleExpandP0<F, Ctx>(result.p0, params, gen, ctx);
+        moduleMvoleGenDelta<F, Ctx>(gen.Delta, params, prng, ctx);
+        moduleMvoleGenRowMasks<F, Ctx>(gen.rowMasks, params, gen.base, gen.Delta, prng, ctx);
+        end = omp_get_wtime();
+        timings.directMaskSeconds = end - start;
+        timings.genSeconds = timings.genBaseSeconds + timings.directMaskSeconds;
+
+        timings.microWhtOneSeconds = moduleMvoleMicrobenchWhtOne<F, Ctx>(params, prng, ctx);
+        timings.microWhtMSeconds = moduleMvoleMicrobenchWhtM<F, Ctx>(params, prng, ctx);
+
+        start = omp_get_wtime();
+        moduleMvoleExpandP0Timed<F, Ctx>(result.p0, params, gen, timings, ctx);
         end = omp_get_wtime();
         timings.expandP0Seconds = end - start;
 
         start = omp_get_wtime();
-        moduleMvoleExpandP1<F, Ctx>(result.p1, params, gen, ctx);
+        moduleMvoleExpandP1Timed<F, Ctx>(result.p1, params, gen, timings, ctx);
         end = omp_get_wtime();
         timings.expandP1Seconds = end - start;
 
@@ -384,9 +571,11 @@ namespace osuCrypto
     bool runModuleMvoleBenchmark(const ModuleMVOLEParams& params, const std::string& label)
     {
         auto timings = moduleMvoleBenchmarkOnce<F, Ctx>(params);
-        auto total = moduleMvoleTotalSeconds(timings);
+        auto totalWithVerify = moduleMvoleTotalSeconds(timings);
+        auto totalNoVerify = moduleMvoleTotalNoVerifySeconds(timings);
         auto outputElems = params.m * params.N;
-        auto throughput = total > 0 ? static_cast<double>(outputElems) / total : 0.0;
+        auto throughputWithVerify = totalWithVerify > 0 ? static_cast<double>(outputElems) / totalWithVerify : 0.0;
+        auto throughputNoVerify = totalNoVerify > 0 ? static_cast<double>(outputElems) / totalNoVerify : 0.0;
 
         std::cout << std::fixed << std::setprecision(6)
                   << "MODULE_MVOLE_BENCH " << label
@@ -394,12 +583,21 @@ namespace osuCrypto
                   << " t=" << params.t
                   << " m=" << params.m
                   << " output_elems=" << outputElems
+                  << " gen_base_s=" << timings.genBaseSeconds
+                  << " direct_masks_s=" << timings.directMaskSeconds
                   << " gen_s=" << timings.genSeconds
                   << " expand_p0_s=" << timings.expandP0Seconds
                   << " expand_p1_s=" << timings.expandP1Seconds
+                  << " x_wht_s=" << timings.xWhtSeconds
+                  << " z0_wht_m_s=" << timings.z0RowWhtSeconds
+                  << " z1_wht_m_s=" << timings.z1RowWhtSeconds
                   << " verify_s=" << timings.verifySeconds
-                  << " total_s=" << total
-                  << " throughput_elems_per_s=" << throughput
+                  << " total_no_verify_s=" << totalNoVerify
+                  << " total_with_verify_s=" << totalWithVerify
+                  << " throughput_no_verify_elems_per_s=" << throughputNoVerify
+                  << " throughput_with_verify_elems_per_s=" << throughputWithVerify
+                  << " micro_wht_1_s=" << timings.microWhtOneSeconds
+                  << " micro_wht_m_s=" << timings.microWhtMSeconds
                   << " " << (timings.ok ? "PASS" : "FAIL")
                   << std::endl;
 
--- docs/module_mvole_benchmark.md
+++ docs/module_mvole_benchmark.md
@@ -6,12 +6,14 @@ These are lightweight local benchmarks for the current ModuleMVOLE algebraic cor
 
 The benchmark mode times:
 
-- local Gen: base ring state, `Delta`, and direct row-mask generation
-- Expand P0: P0 output expansion, including module ring multiplication and WHT work
-- Expand P1: P1 output expansion, including module ring multiplication and WHT work
+- sparse/base Gen: public `rho`, sparse `s/e`, `rho*s + e`, and precomputed `WHT(rho)`
+- direct sharing masks: local random row masks for `Delta*s` and `Delta*e`
+- scalar WHT for `x`
+- row-wise WHT work for `Z0`
+- row-wise WHT work for `Z1`
 - verification: entrywise check of `Z0[h][j] + Z1[h][j] == Delta[h] * x[j]`
 
-The current prototype uses direct `O(N^2)` XOR-convolution ring multiplication, so these timings should not be interpreted as final protocol performance.
+The benchmark also reports totals both with and without verification.
 
 ## Build
 
@@ -25,44 +27,67 @@ cmake --build build --parallel 64
 ./build/main --MODULE_MVOLE_BENCH 1
 ./build/main --MODULE_MVOLE_BENCH 2
 ./build/main --MODULE_MVOLE_BENCH 3
-timeout 30s ./build/main --MODULE_MVOLE_BENCH 4
 ```
 
-## Results
-
-Fp64 was benchmarked first. Fp32 benchmark reporting was not added in this phase because the Fp64 direct-convolution cost already reaches the lightweight limit at `N = 2^14`.
-
-| Command | `N` | `t` | `m` | Output elems `m*N` | Gen s | Expand P0 s | Expand P1 s | Verify s | Total s | Throughput elems/s | Result |
-| --- | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | --- |
-| `./build/main --MODULE_MVOLE_BENCH 1` | 1024 | 8 | 8 | 8192 | 0.003778 | 0.027751 | 0.027681 | 0.000042 | 0.059253 | 138255.594706 | PASS |
-| `./build/main --MODULE_MVOLE_BENCH 2` | 4096 | 16 | 16 | 65536 | 0.057450 | 0.880187 | 0.879864 | 0.000293 | 1.817793 | 36052.501692 | PASS |
-| `./build/main --MODULE_MVOLE_BENCH 3` | 16384 | 32 | 16 | 262144 | 0.887434 | 14.054739 | 14.061557 | 0.004700 | 29.008430 | 9036.821246 | PASS |
-
-Preset `4` maps to `N = 65536`, `t = 64`, `m = 32`. It did not complete within a 30-second cap:
-
-```bash
-timeout 30s ./build/main --MODULE_MVOLE_BENCH 4
-```
-
-Exit code:
-
-```text
-124
-```
-
-The largest successful lightweight benchmark in this phase is therefore preset `3`, `N = 2^14`, `t = 32`, `m = 16`.
-
-## Correctness Check
-
-After adding the benchmark flag, the original Phase 3A/3B correctness command was rerun:
+Correctness presets were also rerun:
 
 ```bash
 ./build/main --MODULE_MVOLE 1
+./build/main --MODULE_MVOLE 2
+./build/main --MODULE_MVOLE 3
 ```
 
-Output:
+## Local optimization applied
+
+The earlier Phase 3C benchmark used direct dense XOR-convolution during every P0/P1 row expansion and then immediately applied WHT to the result. Phase 3D replaces that local computation with the equivalent WHT-domain identity:
+
+```text
+WHT(rho * row + mask) = WHT(rho) * WHT(row) + WHT(mask)
+```
+
+This is not a PPRF/silent setup integration and does not change the ModuleMVOLE correctness relation. It only avoids repeated dense `O(N^2)` row convolutions in the local harness. The base `rho*s` step now also uses a sparse-RHS convolution because `s` is sparse.
+
+## Detailed Results
+
+Fp64 benchmark output:
+
+| Command | `N` | `t` | `m` | Output elems | Gen base s | Direct masks s | Gen total s | Expand P0 s | Expand P1 s | `x` WHT s | `Z0` WHT_m s | `Z1` WHT_m s | Verify s | Total no verify s | Total with verify s | Throughput no verify elems/s | Throughput with verify elems/s | Result |
+| --- | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | ---: | --- |
+| `./build/main --MODULE_MVOLE_BENCH 1` | 1024 | 8 | 8 | 8192 | 0.000072 | 0.000292 | 0.000364 | 0.000205 | 0.000207 | 0.000008 | 0.000121 | 0.000121 | 0.000070 | 0.000776 | 0.000846 | 10552846.481711 | 9682038.250260 | PASS |
+| `./build/main --MODULE_MVOLE_BENCH 2` | 4096 | 16 | 16 | 65536 | 0.000383 | 0.002322 | 0.002704 | 0.001668 | 0.001620 | 0.000032 | 0.000990 | 0.000992 | 0.000285 | 0.005993 | 0.006278 | 10935259.401610 | 10439456.624918 | PASS |
+| `./build/main --MODULE_MVOLE_BENCH 3` | 16384 | 32 | 16 | 262144 | 0.002335 | 0.008831 | 0.011166 | 0.007136 | 0.007001 | 0.000140 | 0.004355 | 0.004411 | 0.001144 | 0.025304 | 0.026448 | 10359886.864787 | 9911717.540682 | PASS |
+
+## WHT Microbenchmarks
+
+The benchmark mode also runs small WHT-only probes on the same field context:
+
+| Command | `N` | `m` | One-vector WHT s | WHT_m s |
+| --- | ---: | ---: | ---: | ---: |
+| `./build/main --MODULE_MVOLE_BENCH 1` | 1024 | 8 | 0.000008 | 0.000105 |
+| `./build/main --MODULE_MVOLE_BENCH 2` | 4096 | 16 | 0.000033 | 0.000849 |
+| `./build/main --MODULE_MVOLE_BENCH 3` | 16384 | 16 | 0.000141 | 0.003612 |
+
+## Correctness Check
+
+Output from the correctness presets after the optimization:
 
 ```text
 MODULE_MVOLE Fp64 N=256 t=4 m=4 PASS
 MODULE_MVOLE Fp32 N=256 t=4 m=4 PASS
+MODULE_MVOLE Fp64 N=1024 t=8 m=8 PASS
+MODULE_MVOLE Fp32 N=1024 t=8 m=8 PASS
+MODULE_MVOLE Fp64 N=2048 t=16 m=8 PASS
+MODULE_MVOLE Fp32 N=2048 t=16 m=8 PASS
 ```
+
+## Bottleneck Diagnosis
+
+Before Phase 3D, preset `3` (`N = 2^14`, `t = 32`, `m = 16`) took about 29 seconds because each row expansion performed dense XOR-convolution before WHT. That dense convolution dominated the cost.
+
+After the local WHT-domain optimization, preset `3` completes in about 0.026 seconds including verification. The remaining costs are mainly:
+
+- direct random mask generation, about 0.0088 seconds
+- row-wise WHT and output expansion for `Z0/Z1`, about 0.0141 seconds combined
+- verification, about 0.0011 seconds
+
+This benchmark is still only for the algebraic local harness. The next meaningful bottleneck will come from the real PPRF/silent setup once that path is integrated.
